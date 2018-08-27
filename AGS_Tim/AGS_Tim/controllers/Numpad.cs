3be1fb475064f29f2c5e40139504d2cb9452c297
Hannes_R_Richter <Hannes-R-Richter@gmx.de>
2018-08-27 12:18:21 +0200

    Interface implementiert

--- AGS_Tim/AGS_Tim/controllers/Numpad.cs
+++ AGS_Tim/AGS_Tim/controllers/Numpad.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace AGS_Tim.controllers
 {
-    class Numpad
+    public class Numpad : InputController
     {
+        public bool Available { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler<InputController> Disconnected;
+        public event EventHandler<int> ButtonPressed;
     }
 }
