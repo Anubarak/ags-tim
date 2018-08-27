1cfd610f3dcb1f3dd492f62c711987ed206deda2
boettcher <h.c.boettcher@gmx.de>
2018-08-27 13:06:02 +0200

    1

--- AGS_Tim/AGS_Tim/services/Database.cs
+++ AGS_Tim/AGS_Tim/services/Database.cs
@@ -1,21 +1,48 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data.SQLite;
+
 
 
 namespace AGS_Tim.services
 {
     class Database
     {
+        private SQLiteConnection dbConnection;  
         public  Database() {
+          dbConnection = new SQLiteConnection("Data Source=TIM.db;Version=3;"); 
+        }
 
-       
+        public void OpenDatabase()
+        {
+            try
+            {
+                dbConnection.Open();
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+
+        public int Execute()
+        {
+            try
+            {
            
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return 0;
         }
 
         
