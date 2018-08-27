06408d9122ad858c4988516e21b826d130b71c79
robin schambach <schambach@secondred.de>
2018-08-27 11:51:49 +0200

    include static Main class

---
+++ AGS_Tim/AGS_Tim/Main.cs
@@ -0,0 +1,24 @@
+﻿using AGS_Tim.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGS_Tim
+{
+    static class Main
+    {
+
+        public static Database db;
+        public static Highscores highscores;
+        public static Questions questions;
+
+        static void init()
+        {
+            db = new Database();
+            highscores = new Highscores();
+            questions = new Questions();
+        }
+    }
+}
