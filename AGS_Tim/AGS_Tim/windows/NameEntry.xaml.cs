7ad0c3f75ca41a4fd34708a88e2689643bc87c07
Stefan Anschütz <sanschuetz86@gmail.com>
2018-08-28 12:51:15 +0200

    ButtonPressed update #2

--- AGS_Tim/AGS_Tim/windows/NameEntry.xaml.cs
+++ AGS_Tim/AGS_Tim/windows/NameEntry.xaml.cs
@@ -25,6 +25,16 @@ namespace AGS_Tim.windows
             InitializeComponent();
         }
 
+        public void ButtonPressed(int ButtonNumber)
+        {
+            if (ButtonNumber == 1)
+            {
+                Main.mainWindow.Content = Main.mainWindow.GetGame();
+            }
+            else
+            {
 
+            }
+        }
     }
 }
