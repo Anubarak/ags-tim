0a6275f0f93bb2d2921e8154869f7104fd0dff5d
Hannes_R_Richter <Hannes-R-Richter@gmx.de>
2018-08-28 08:41:49 +0200

    HW Implementation begonnen, Verweise hinzugefügt

--- AGS_Tim/AGS_Tim/controllers/Numpad.cs
+++ AGS_Tim/AGS_Tim/controllers/Numpad.cs
@@ -1,16 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AGS_Tim.models;
+using System;
+using System.Management;
+using System.Threading;
 
 namespace AGS_Tim.controllers
 {
     public class Numpad : InputController
     {
-        public bool Available { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Numpad()
+        {
+            
+        }
 
-        public event EventHandler<InputController> Disconnected;
+        /// <summary> Methode für Threadaufruf. Prüft ob die Hardwarekomponente verfügbar ist </summary>
+        private void checkAvailability()
+        {
+            bool oldAvailable = Available;
+            while(true)
+            {
+                if (Available != oldAvailable)
+                {
+                    if (Disconnected != null)
+                        Disconnected(this, EHWInput.Keyboard);
+                }
+            }
+        }
+
         public event EventHandler<int> ButtonPressed;
+
+        /// <summary> Event welches beim Disconnect der Harware feuert </summary>
+        public event EventHandler<EHWInput> Disconnected;
+        public event EventHandler<EHWInput> Connected;
+
+        /// <summary> Gibt an ob die Hardware verfügbar ist </summary>
+        public bool Available
+        {
+            get
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select Name from Win32_Keyboard");
+                foreach (ManagementObject keyboard in searcher.Get())
+                {
+                    if (!keyboard.GetPropertyValue("Name").Equals(""))
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        Thread availabilityThread;
     }
 }
