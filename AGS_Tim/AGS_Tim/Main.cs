e96c0c63dac30bd897bdd12ac139e7043a649400
boettcher <h.c.boettcher@gmx.de>
2018-08-28 13:14:47 +0200

    Tim.db

--- AGS_Tim/AGS_Tim/Main.cs
+++ AGS_Tim/AGS_Tim/Main.cs
@@ -17,7 +17,8 @@ namespace AGS_Tim
         public static Questions questions;
         public static Subjects subjects; 
         public static MainWindow mainWindow;
-        public static GameSession gameSession; 
+        public static GameSession gameSession;
+        public static Hardware hardware;
 
        public  static void init()
         {
@@ -26,12 +27,11 @@ namespace AGS_Tim
             questions = new Questions();
             subjects = new Subjects(); 
             mainWindow = new MainWindow();
-        
+            hardware = new Hardware(mainWindow);
+
             //Zum Testen
-            gameSession = new GameSession(); 
-  
-      
-     
+            gameSession = new GameSession();
+
         }
     }
 }
