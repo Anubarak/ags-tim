2bcef632f2c44c16f5b4ae25b2386867b64621ce
Stefan Anschütz <sanschuetz86@gmail.com>
2018-08-29 11:45:05 +0200

    Game.xaml update

--- AGS_Tim/AGS_Tim/windows/Game.xaml.cs
+++ AGS_Tim/AGS_Tim/windows/Game.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using AGS_Tim.models;
 namespace AGS_Tim.windows
 {
     /// <summary>
@@ -20,6 +20,10 @@ namespace AGS_Tim.windows
     /// </summary>
     public partial class Game : Page
     {
+        bool IsAnswering = false;
+        int CurrentTable = 1;
+        Player activePlayer;
+
         public Game()
         {
             InitializeComponent();
@@ -27,7 +31,136 @@ namespace AGS_Tim.windows
 
         public void ButtonPressed(int ButtonNumber)
         {
+            if (IsAnswering)
+            {
+                
+            }
+            else
+            {
+                if (ButtonNumber == 1) {
+                    this.IsAnswering = true;
 
+                    activePlayer =  Main.gameSession.gs.players[CurrentTable - 1];
+                    this.TbQuestion.Text = activePlayer.Question.text;
+
+                }
+                else if (ButtonNumber == 2 || ButtonNumber == 8)
+                {
+                    switch (CurrentTable)
+                    {
+                        case 1:
+                            ChangeTable(4);
+                            break;
+                        case 2:
+                            ChangeTable(5);
+                            break;
+                        case 3:
+                            ChangeTable(6);
+                            break;
+                        case 4:
+                            ChangeTable(1);
+                            break;
+                        case 5:
+                            ChangeTable(2);
+                            break;
+                        case 6:
+                            ChangeTable(3);
+                            break;
+                    }
+                }
+                else if (ButtonNumber == 4)
+                {
+                    switch (CurrentTable)
+                    {
+                        case 1:
+                            ChangeTable(3);
+                            break;
+                        case 2:
+                            ChangeTable(1);
+                            break;
+                        case 3:
+                            ChangeTable(2);
+                            break;
+                        case 4:
+                            ChangeTable(6);
+                            break;
+                        case 5:
+                            ChangeTable(4);
+                            break;
+                        case 6:
+                            ChangeTable(5);
+                            break;
+                    }
+                }
+                else if (ButtonNumber == 6)
+                {
+                    switch (CurrentTable)
+                    {
+                        case 1:
+                            ChangeTable(2);
+                            break;
+                        case 2:
+                            ChangeTable(3);
+                            break;
+                        case 3:
+                            ChangeTable(1);
+                            break;
+                        case 4:
+                            ChangeTable(5);
+                            break;
+                        case 5:
+                            ChangeTable(6);
+                            break;
+                        case 6:
+                            ChangeTable(4);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void ChangeTable(int activate)
+        {
+            DeactivateTable();
+            this.CurrentTable = activate;
+            switch (activate)
+            {
+                case 1:
+                    this.BorderTable1.Visibility = Visibility.Visible;
+                    break;
+                case 2:
+                    this.BorderTable2.Visibility = Visibility.Visible;
+                    break;
+                case 3:
+                    this.BorderTable3.Visibility = Visibility.Visible;
+                    break;
+                case 4:
+                    this.BorderTable4.Visibility = Visibility.Visible;
+                    break;
+                case 5:
+                    this.BorderTable5.Visibility = Visibility.Visible;
+                    break;
+                case 6:
+                    this.BorderTable6.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void DeactivateTable()
+        {
+            this.BorderTable1.Visibility = Visibility.Hidden;
+            this.BorderTable2.Visibility = Visibility.Hidden;
+            this.BorderTable3.Visibility = Visibility.Hidden;
+            this.BorderTable5.Visibility = Visibility.Hidden;
+            this.BorderTable4.Visibility = Visibility.Hidden;
+            this.BorderTable6.Visibility = Visibility.Hidden;
+
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Main.gameSession.gs.startTime = DateTime.Now;
+
+            this.BorderTable1.Visibility = Visibility.Visible;
         }
     }
 }
