bb031c96066069d93b32f6260adb6c949f567bb4
Stefan Anschütz <sanschuetz86@gmail.com>
2018-08-30 09:07:19 +0200

    room.png hinzugefügt

--- AGS_Tim/AGS_Tim/Properties/Resources.Designer.cs
+++ AGS_Tim/AGS_Tim/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace AGS_Tim.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap room {
+            get {
+                object obj = ResourceManager.GetObject("room", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap tafel {
             get {
                 object obj = ResourceManager.GetObject("tafel", resourceCulture);
